a09b01836a5cef5ef045bed378727a1b048d97ef
bhavuk.arora <bhavuk.arora@rib-software.com>
2024-09-07 11:29:30 +0200

    Changes

--- fleetManagementApp/routes/dashboard.js
+++ fleetManagementApp/routes/dashboard.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const dashboardController = require('../controllers/dashboard');
 
 router.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect('http://localhost:3000/dashboard');
-  } else {
-    res.redirect('http://localhost:3000/login');
-  }
-});
+  res.send("hello");
+})
+
+// router.get('/', (req, res) => {
+//   if (req.isAuthenticated()) {
+//     res.redirect('http://localhost:3000/dashboard');
+//   } else {
+//     res.redirect('http://localhost:3000/login');
+//   }
+// });
 
 // New route to fetch vehicle performance data
 router.get('/data', dashboardController.getVehiclePerformanceData);
