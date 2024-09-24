a0ec3564d4ce9df147b4ecb14ab15fe83c6e21d8
bhavuk.arora <bhavuk.arora@rib-software.com>
2024-09-24 04:01:24 +0200

    changes

--- fleetManagementApp/routes/login.js
+++ fleetManagementApp/routes/login.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const loginController = require('../Controllers/login');
+const loginController = require('../controllers/login');
 
 
 // Local login route
